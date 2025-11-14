596e63bb8b0b9a855b47546afa480393cc0a7570
dazdaz <darren@dazdaz.org>
2025-11-14 17:45:04 +0100

    Update artist1.sh to use relative path for setup_music.py

--- artist1.sh
+++ artist1.sh
@@ -1,1 +1,1 @@
-setup_music.py https://palochebeatz.bandcamp.com/album/ozzy-osbourne-guitar-backing-tracks
+./setup_music.py https://palochebeatz.bandcamp.com/album/ozzy-osbourne-guitar-backing-tracks
